c0485643e59bafe8b62b817304c7443cc7ccee4e
Alexandra Chaniotaki <achaniotaki@dmst.aueb.gr>
2016-12-29 18:31:39 +0200

    2η έκδοση Parsing
    Η δεύτερη έκδοση του parsing των λέξεων με τη βοήθεια της μεθόδου replaceAll. (Αντικατάσταση των tag και των άχρηστων πληροφοριών της HTML με κενά). Επιπλέον, αφαιρούνται οι stop words.

--- WordsUrlParsing.java
+++ WordsUrlParsing.java
@@ -1,87 +1,102 @@
-import java.util.*;
 import java.net.*;
 import java.io.*;
 
-
 public class WordsUrlParsing {
-  public static void main(String args[]) throws Exception {
-    try {
-      URL oracle = new URL("http://edition.cnn.com/2016/11/18/tech/how-to-spot-fake-misleading-news-trnd/index.html");
-      BufferedReader in = new BufferedReader(
-      new InputStreamReader(oracle.openStream()));
 
-      String input, texturl="";
-      while ((input = in.readLine()) != null)
-      texturl += input;
-      in.close();
+	private static final String[] stopWords = { "a", "about", "above", "above", "across", "after", "afterwards",
+			"again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am",
+			"among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything",
+			"anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes",
+			"becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between",
+			"beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con", "could",
+			"couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight",
+			"either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone",
+			"everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five",
+			"for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give",
+			"go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein",
+			"hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in",
+			"inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly",
+			"least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more",
+			"moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never",
+			"nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere",
+			"of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our",
+			"ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re",
+			"same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side",
+			"since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime",
+			"sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them",
+			"themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon",
+			"these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout",
+			"thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un",
+			"under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever",
+			"when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon",
+			"wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why",
+			"will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves",
+			"the" };
 
-      String str = "", str2 = "", str3 = "", str4 = "", str5 = "";
-      boolean check = false, script = false, comment = false, style = false;
-      int special = 2, len3 = 0, len4 = 0, len5 = 0;
-      String[] words;
+	public static void main(String args[]) throws Exception {
+		try {
 
-      for (char ch: texturl.toCharArray()) {
-        if (comment == true) {
-          str4 += ch;
-          len4 = str4.length();
-          if (len4 > 4 && str4.substring(len4-3, len4).equals("-->")) {
-            comment = false;
-            str4 = "";
-            len4 = 0;
-          }
-        }
-        if (script == true && comment == false) {
-          str3 += ch;
-          len3 = str3.length();
-          if (len3 > 7 && str3.substring(len3-8, len3).equals("/script>")) {
-            script = false;
-            str3 = "";
-            len3 = 0;
-          }
-        }
-        if (style == true && comment == false) {
-          str5 += ch;
-          len5 = str5.length();
-          if (len5 > 6 && str5.substring(len5-7, len5).equals("/style>")) {
-            style = false;
-            str5 = "";
-            len5 = 0;
-          }
-        }
-        if (ch=='>' && special%2 == 0 && comment == false && script == false && style == false) {
-          if (str2.indexOf("script") == 0) {
-            script = true;
-          }
-          if (str2.indexOf("style") == 0) {
-            style = true;
-          }
-          check = true;
-          str2 = "";
-        } else if ((ch =='-') && (str2.indexOf("!-") == 0) && comment == false) {
-            comment = true;
-        }
-        else if (ch=='<' && comment == false && script == false && style == false) {
-          if (str!="") {
-            words = str.split("\\s+");
-            for (String word : words) {
-              if (!word.equals("") && !word.substring(word.length() - 1, word.length()).equals(";")) {
-                System.out.println(word);
-              }
-            }
-          }
-          check=false;
-          str = "";
-        } else if (check==true && script==false && comment == false && style == false) {
-          str += ch;
-        } else if (check==false && comment == false) {
-          str2 += ch;
-        }
-        if (ch=='\"') {
-          special++;
-        }
-      }
-    } catch(Exception e1) {
+			URL inputURL = new URL("http://edition.cnn.com/");
+			BufferedReader in = new BufferedReader(new InputStreamReader(inputURL.openStream(), "UTF8"));
 
-    }
-  }
+			String input, texturl = "";
+			while ((input = in.readLine()) != null)
+				texturl += input.toLowerCase();
+			in.close();
+
+			texturl = texturl.replaceAll("\"<\"", "");
+			texturl = texturl.replaceAll("\">\"", "");
+			texturl = texturl.replaceAll("<!--.+?-->", " ");
+			texturl = texturl.replaceAll("<script.+?</script>", " ");
+			texturl = texturl.replaceAll("<style.+?/style>", " ");
+			texturl = texturl.replaceAll("<iframe.+?/iframe>", " ");
+			texturl = texturl.replaceAll("<[^><]*>", " ");
+			texturl = texturl.replaceAll("[`~!@#$%^&*()_+\\;\',./{}|:\"<>?]", " ");
+			texturl = modifyString(texturl);
+			texturl = texturl.replaceAll("[0-9]", " ");
+
+			String[] a;
+			a = texturl.split("\\s+");
+
+			for (int i = 0; i < a.length; i++) {
+				for (int j = 0; j < stopWords.length; j++) {
+					if (a[i].equals(stopWords[j])) {
+						a[i] = "";
+						break;
+					}
+				}
+				if (a[i].length() == 1) {
+					a[i] = "";
+				}
+			}
+
+			for (String word : a) {
+				if (!word.equals("")) {
+					System.out.println(word);
+				}
+			}
+			System.out.printf("%d", a.length);
+			System.out.println();
+		} catch (Exception e1) {
+			System.out.println(e1);
+		}
+	}
+
+	public static String modifyString(String htmlCode) {
+		String strToConvert = "";
+		boolean status = false;
+
+		for (char ch : htmlCode.toCharArray()) {
+			if (ch == '&' || status == true) {
+				strToConvert += ch;
+				status = true;
+				if (ch == ';') {
+					htmlCode = htmlCode.replace(strToConvert, " ");
+					strToConvert = "";
+					status = false;
+				}
+			}
+		}
+		return htmlCode;
+	}
 }
