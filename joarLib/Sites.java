dbc143c837a80000625be0cd6878620eb02472e2
irokafetzaki <iro.kafetzaki@gmail.com>
2017-01-09 16:46:18 +0200

    προσθήκη javadoc στα αρχεία Sites,Siteword,Word

--- joarLib/Sites.java
+++ joarLib/Sites.java
@@ -1,55 +1,84 @@
-package joarLib;
-
+/**
+* The Sites program implements the initialization of id_site,site,title and description
+* variables and the methods to set and return these variables.
+*
+* @author  J0arTeam
+* @version 1.0
+* @since   2017-01-03
+*/
 public class Sites {
-
+ /** Η μεταβλητή με έναν αύξοντα αριθμό για τα site*/
   private int id_site;
-	private String site,title,description;
+  /** Η μεταβλητή με το url του site*/
+  private String site;
+  /** Η μεταβλητή με τον τίτλο του site που δίνεται από την html*/
+  private String title;
+  /** Η μεταβλητή με την περιγραφή του site που δίνονται από τα <meta> tags της html */
+  private String description;
 
-	public Sites(int id_site, String site, String title, String description ) {
-		this.site = site;
-        this.id_site = id_site;
-        this.title = title;
-        this.description = description;
-	}
 
-  public Sites(String site , String title, String description) {
+   /** Constructor to initiliaze the id_site,site,title and description variables*/
+	public Sites(int id_site, String site, String title, String description) {
+    this.id_site = id_site;
     this.site = site;
     this.title = title;
-     this.description = description;
+    this.description = description;
+	}
+ /**Consructor to initialize only the site variable*/
+  public Sites(String site) {
+    this.site = site;
   }
 
-    public Sites(String site) {
-      this.site = site;
-  }
-
+/**
+ * @return current site
+ */
 	public String getSite() {
 		return site;
 	}
-
+/**
+ *
+ * @param site site to set
+ */
 	public void setSite(String site) {
 		this.site = site;
 	}
-
-	public int getIdSite() {
-		return id_site;
-	}
-
-	public void setIdSite(int id_site) {
-		this.id_site = id_site;
-	}
-	public String getTitle() {
+/**
+ * @return current title
+ */
+  public String getTitle() {
 		return title;
 	}
-
+/**
+ *
+ * @param title title to set
+ */
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-	public String getDescription() {
-		return description;
+/**
+ * @return current description
+ */
+  public String getDescription() {
+    return description;
+  }
+/**
+ *
+ * @param description description to set
+ */
+  public void setDescription(String description) {
+    this.description = description;
+  }
+/**
+ * @return current id_site
+ */
+	public int getIdSite() {
+		return id_site;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
+/**
+ *
+ * @param id_site id_site to set
+ */
+	public void setIdSite(int id_site) {
+		this.id_site = id_site;
 	}
 }
